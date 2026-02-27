75ea2e2c8043869a7ee62425bdf24a7df7cfd963
ARUN KUMAR.A <86552741+Arun12kumar@users.noreply.github.com>
2026-02-27 07:16:56 +0530

    commit
    server.js file and package install

---
+++ src/config/databade.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const development ={
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'lead_management_db',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: console.log,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+}
+
+export const production= {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 10,
+      min: 2,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
--- src/server.js
+++ src/server.js
@@ -1,14 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
+import swagger from 'swagger-ui-express';
+import { sequelize } from './config/db.js';
+//const logger from ''
 
-
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
 const app = express();
+
+//Midileware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// app.use(morgan('combined',{
+//     stream:{write: (msg) => lo}
+// }))
 
 app.get('/health',(req,res)=>{
     res.send("working good✅")
 })
 
-app.listen(PORT,()=>{
-    console.log(`server is running on http://localhost:${PORT}`)
-})
+const startServer = async () =>{
+    try {
+        await sequelize.authenticate();
+
+        app.listen(PORT,()=>{
+            console.log(`server running on http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        process.exit(1)
+    }
+}
+
+startServer();
